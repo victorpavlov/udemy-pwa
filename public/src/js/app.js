cbb8ae536ca2f1edf3067ee2700ff86611ca61b6
Victor Pavlov <vitekpavlov@gmail.com>
2020-04-28 19:49:33 +0300

    Lecture 146 passed.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,6 @@
 
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -22,3 +23,29 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  var options = {
+    body: 'Some body text in the notification.'
+  };
+  new Notification('Successfully Subscribed!', options);
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function(result) {
+    console.log('User Choice', result);
+    if (result !== 'granted') {
+      console.log('No permission granted');
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ('Notification' in window) {
+  for (var i = 0; i < enableNotificationsButtons.length; i += 1) {
+    var button = enableNotificationsButtons[i];
+    button.style.display = 'inline-block';
+    button.addEventListener('click', askForNotificationPermission);
+  }
+}
