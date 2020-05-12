afa568e6d0386a564bf4b17c860cb2470479317e
Victor Pavlov <vitekpavlov@gmail.com>
2020-05-12 19:43:55 +0300

    Lecture 186 passed.

---
+++ public/service-worker.js
@@ -0,0 +1,102 @@
+importScripts('workbox-sw.prod.v2.1.3.js');
+
+const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cache: 'google-fonts'
+}));
+
+workboxSW.precache([
+  {
+    "url": "404.html",
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "2cab47d9e04d664d93c8d91aec59e812"
+  },
+  {
+    "url": "index.html",
+    "revision": "fd48136656e4e3192e3a1e6214e6a965"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "d11c7965f5cfba711c8e74afa6c703d7"
+  },
+  {
+    "url": "offline.html",
+    "revision": "45352e71a80a5c75d25e226e7330871b"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "be5054de6f45ad8463a0f958ad5be038"
+  },
+  {
+    "url": "src/css/app.css",
+    "revision": "f27b4d5a6a99f7b6ed6d06f6583b73fa"
+  },
+  {
+    "url": "src/css/feed.css",
+    "revision": "2645a44cd2641297ab72694b9ed113ff"
+  },
+  {
+    "url": "src/css/help.css",
+    "revision": "1c6d81b27c9d423bece9869b07a7bd73"
+  },
+  {
+    "url": "src/js/app.js",
+    "revision": "3aab3e5f6c29359976b1e2b7083fe7ea"
+  },
+  {
+    "url": "src/js/feed.js",
+    "revision": "7b866a4265f3e735eba353d1433987bf"
+  },
+  {
+    "url": "src/js/fetch.js",
+    "revision": "6b82fbb55ae19be4935964ae8c338e92"
+  },
+  {
+    "url": "src/js/idb.js",
+    "revision": "017ced36d82bea1e08b08393361e354d"
+  },
+  {
+    "url": "src/js/material.min.js",
+    "revision": "713af0c6ce93dbbce2f00bf0a98d0541"
+  },
+  {
+    "url": "src/js/promise.js",
+    "revision": "10c2238dcd105eb23f703ee53067417f"
+  },
+  {
+    "url": "src/js/utility.js",
+    "revision": "3913a747ceee8149da360c4ef0311649"
+  },
+  {
+    "url": "sw-base.js",
+    "revision": "10105f1cc6a18a0c6e28a4468be0114b"
+  },
+  {
+    "url": "sw.js",
+    "revision": "05c460eb78f0cfeb9e568e6d5dd6a195"
+  },
+  {
+    "url": "workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  },
+  {
+    "url": "src/images/main-image-lg.jpg",
+    "revision": "31b19bffae4ea13ca0f2178ddb639403"
+  },
+  {
+    "url": "src/images/main-image-sm.jpg",
+    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
+  },
+  {
+    "url": "src/images/main-image.jpg",
+    "revision": "5c66d091b0dc200e8e89e56c589821fb"
+  },
+  {
+    "url": "src/images/sf-boat.jpg",
+    "revision": "0f282d64b0fb306daf12050e812d6a19"
+  }
+]);
---
+++ public/sw-base.js
@@ -0,0 +1,9 @@
+importScripts('workbox-sw.prod.v2.1.3.js');
+
+const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+  cache: 'google-fonts'
+}));
+
+workboxSW.precache([]);
